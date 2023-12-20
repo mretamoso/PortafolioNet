17a961c7a16c186d000b4edf8f7626378602218b
Miguel Alonso <miguelretamosoin@gmail.com>
2023-12-20 02:01:02 -0500

    Se agrego formulario de contacto

--- PortafolioNet/Controllers/HomeController.cs
+++ PortafolioNet/Controllers/HomeController.cs
@@ -38,6 +38,11 @@ namespace PortafolioNet.Controllers
             return View(proyectos);
         }
 
+        public IActionResult Contacto()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
