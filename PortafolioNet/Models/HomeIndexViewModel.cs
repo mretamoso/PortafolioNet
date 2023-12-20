792eefa183ff9549ce2884025f10532852e666b5
Miguel Alonso <miguelretamosoin@gmail.com>
2023-12-20 01:12:38 -0500

    Proyectos agregados...

--- PortafolioNet/Models/HomeIndexViewModel.cs
+++ PortafolioNet/Models/HomeIndexViewModel.cs
@@ -3,5 +3,8 @@
     public class HomeIndexViewModel
     {
         public IEnumerable<ProyectoDTO> Proyectos { get; set; }
+        public EjemploGuidViewModel EjemploGUID_1 { get; set; }
+        public EjemploGuidViewModel EjemploGUID_2 { get; set; }
+        public EjemploGuidViewModel EjemploGUID_3 { get; set; }
     }
 }
